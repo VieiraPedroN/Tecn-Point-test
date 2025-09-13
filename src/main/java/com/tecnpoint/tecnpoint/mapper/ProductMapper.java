a1cf7475e9cddf15f9e03ac7ca8a3cb6bf4264bb
Pedro Vieira <pedronicolas576@gmail.com>
2025-09-13 15:31:51 -0300

    Including Mapper

--- src/main/java/com/tecnpoint/tecnpoint/mapper/ProductMapper.java
+++ src/main/java/com/tecnpoint/tecnpoint/mapper/ProductMapper.java
@@ -1,4 +1,23 @@
 package com.tecnpoint.tecnpoint.mapper;
 
+import java.util.List;
+import com.tecnpoint.tecnpoint.dto.*;
+import com.tecnpoint.tecnpoint.entities.*;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring")
 public interface ProductMapper {
+
+    @Mapping(target = "productVariations", qualifiedByName = "mapProductVariationToRecoveryProductVariationDto")
+    RecoveryProductDto mapProductToRecoveryProductDto(Product product);
+
+    @Named("mapProductVariationToRecoveryProductVariationDto")
+    @IterableMapping(qualifiedByName = "mapProductVariationToRecoveryProductVariationDto")
+    List<RecoveryProductVariationDto> mapProductVariationToRecoveryProductVariationDto(List<ProductVariation> productVariations);
+
+    @Named("mapProductVariationToRecoveryProductVariationDto")
+    RecoveryProductVariationDto mapProductVariationToRecoveryProductVariationDto(ProductVariation productVariation);
+
+    ProductVariation mapCreateProductVariationDtoToProductVariation(CreateProductVariationDto createProductVariationDto);
+
 }
